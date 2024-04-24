422051e37f856594d11161e1385e759dd1a66651
yaswanth <yeswanthdev999@gmail.com>
2024-04-24 15:06:24 +0530

    cliecnt side created

--- server/server.mjs
+++ server/server.mjs
@@ -1,17 +1,17 @@
 //const express=require('express');
 //package imports
-import express from 'express';
-import 'express-async-errors';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
 //files imports
 import connectDB from "./config/db.mjs";
 //routes import
-import testroutes from './routes/testroutes.mjs';
-import authroutes from './routes/authroutes.mjs';
-import errormiddleware from './middlewares/errormiddleware.js';
+import testroutes from "./routes/testroutes.mjs";
+import authroutes from "./routes/authroutes.mjs";
+import errormiddleware from "./middlewares/errormiddleware.js";
 
 dotenv.config();
 //mongodb connection
@@ -21,15 +21,18 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 //routes
-app.use('/api/v1/test', testroutes);
-app.use('/api/v1/auth', authroutes);
+app.use("/api/v1/test", testroutes);
+app.use("/api/v1/auth", authroutes);
 //validation middleware
 app.use(errormiddleware);
 //port
 const PORT = process.env.PORT || 4000;
 //listen
 app.listen(PORT, () => {
-    console.log(`node server running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan.white);
+  console.log(
+    `node server running in ${process.env.DEV_MODE} mode on port no ${PORT}`
+      .bgCyan.white
+  );
 });
