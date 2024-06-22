1e09f11db7281009c146d2bce93c0b5e6ba13421
yaswanth-dev143 <yaswanthdev999@gmail.com>
2024-06-22 15:08:30 +0530

    All project files deleted due to incomplete refernce - new project will be uploaded shortly

--- server/controllers/testcontroller.js
+++
@@ -1,4 +0,0 @@
-export const testpostcontroller=(req,res)=>{
-    const {name}=req.body
-    res.status(200).send(`your name is ${name}`)
-};
--- server/routes/testroutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {testpostcontroller} from '../controllers/testcontroller.js';
-import {userAuth} from '../middlewares/authMiddleware.js';
-//router object
-const router=express.Router();
-//routes
-router.post('/test-post', userAuth , testpostcontroller);
-//export
-export default router
